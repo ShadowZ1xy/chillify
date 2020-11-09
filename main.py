12e9c846cfd7293ad320b48d956b55371f51bca6
ShadowZ1xy <shadow.z1xy@gmail.com>
2020-11-09 21:48:42 +0400

    some code cleanup

--- main.py
+++ main.py
@@ -1,19 +1,76 @@
-from threading import Timer
+# from threading import Timer
+# from settings_directory import settings_gui as gui
+# i = 0
+#
+#
+# def test():
+#     global i
+#     print(i)
+#     i += 1
+#
+#
+# while True:
+#     b = Timer(4, test)
+#     b.run()
+#     gui.settings_window_run()
 
-import win32api
-
-mbox = win32api.MessageBox(0, 'hello', 'title')
-mbox.close()
-
-
-def test():
-    print("hello")
-
-
-def close():
-    mbox.close()
-
-
-while True:
-    b = Timer(4, close)
-    b.run()
+# import config as c
+# import timer_gui as gui
+#
+# gui.show_timer_window(5,
+#                       c.alert_window_width,
+#                       c.alert_window_height,
+#                       c.time_font_color,
+#                       c.time_font_and_size,
+#                       c.background_color,
+#                       "Просто приляг и поспи",
+#                       c.hint_text_color,
+#                       c.hint_text_font_and_size,
+#                       c.background_color,
+#                       c.background_color)
+#
+# import threading
+# import time
+#
+#
+# def thread_func_1(some_int):
+#     i = 0
+#     while 1:
+#         print(some_int, i)
+#         i += 1
+#         time.sleep(3)
+#
+#
+# def thread_func_2(some_str):
+#     i = 0
+#     while 2:
+#         print(some_str, i)
+#         i += 1
+#         time.sleep(1.5)
+#
+#
+# test = "test"
+# temp = "temp"
+# thread_1 = threading.Thread(target=thread_func_1, args=(test,))
+# thread_2 = threading.Thread(target=thread_func_2, args=(temp,))
+#
+# thread_1.start()
+# thread_2.start()
+# thread_1.join()
+# thread_2.join()
+#
+# import pystray
+# from PIL import Image, ImageDraw
+#
+# def create_image():
+#     # Generate an image and draw a pattern
+#     image = Image.new('RGB', (50, 50), "blue")
+#     dc = ImageDraw.Draw(image)
+#     dc.rectangle(
+#         (10 // 2, 0, 10, 10 // 2),
+#         fill="black")
+#     return image
+#
+# icon = pystray.Icon('test name')
+# icon.icon = create_image()
+# icon.run()
--- settings_directory/settings_gui.py
+++ settings_directory/settings_gui.py
@@ -6,12 +6,19 @@ from settings_directory import settings
 from settings_directory import settings_controller as controller
 from settings_directory import settings_gui_layout as gui_layout
 
-if __name__ == "__main__":
+
+def settings_window_run():
     loaded_settings = settings.get_settings()
+    current_program_list = os_tool.get_program_list()
     program_list = loaded_settings[config.keyword_program_list]
-    layout = gui_layout.get_layout(sg, os_tool.get_program_list(),
-                                   program_list,
-                                   loaded_settings[config.keyword_duration],
-                                   loaded_settings[config.keyword_periodicity])
+    duration = loaded_settings[config.keyword_duration]
+    periodicity = loaded_settings[config.keyword_periodicity]
+
+    layout = gui_layout.get_layout(sg, current_program_list,
+                                   program_list, duration, periodicity)
+
     window = sg.Window(config.settings_window_title, layout)
     controller.event_controller(window, program_list)
+
+if __name__ == "__main__":
+    settings_window_run()
